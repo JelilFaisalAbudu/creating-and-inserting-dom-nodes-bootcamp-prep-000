fa551e2ea6ad7748c9adac9fa5f9cfa1e13bf327
Jelil Abudu <abudujelilfaisal@gmail.com>
2020-10-19 19:20:56 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+
+
+let element = document.createElement('div');
+document.body.appneChild(element);
+
+let 
